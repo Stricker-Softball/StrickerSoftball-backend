83df7a71d8eb7626ef1904e5eea609a7c48a46e0
Nick Stricker <strickerdev@gmail.com>
2024-08-27 17:19:26 -0500

    softball backend send email

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const server = express();
+const axios = require('axios');
 
 server.use(helmet());
 server.use(cors());
@@ -17,6 +18,43 @@ server.use("/api/events", eventsRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({ msg: 'Server Up!' });
+});
+
+
+
+async function sendEmail(subject, message) {
+  const data = JSON.stringify({
+    "Messages": [{
+      "From": {"Email": "strickerbro@gmail", "Name": "Porfolio Site"},
+      "To": [{"Email": "strickerbro@gmail", "Name": "Nick Stricker"}],
+      "Subject": subject,
+      "TextPart": message
+    }]
+  });
+
+  const config = {
+    method: 'post',
+    url: 'https://api.mailjet.com/v3.1/send',
+    data: data,
+    headers: {'Content-Type': 'application/json'},
+    auth: {username: '68adf2ba533ef188c81c6c6567b9fc8d', password: '4de072936905bfd10853fc921a37127a'},
+  };
+
+  return axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
+
+// define your own email api which points to your server.
+app.post('/api/sendemail/', function (req, res) {
+  const {name, email, message} = req.body;
+  //implement your spam protection or checks.
+  sendEmail(name + " - " + email, message);
 });
 
 module.exports = server;
